5ac3d10658b8d63070b819ae47290505c99bb6e7
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-09-29 14:39:44 -0700

    all inputs working, and have a click counter

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-// import functions and grab DOM elements
+const fullName = document.getElementById('full-name');
 
-// initialize global state
+const button = document.getElementById('generate');
+const yourName = document.getElementById('your-name');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+let count = 0;
+const display = document.getElementById('display');
+
+
+button.addEventListener('click', ()=> {
+    let newName = yourName.value;
+    fullName.textContent = newName;
+    count ++ ;
+    display.innerHTML = count ;
+});
+
+const colorButton = document.getElementById('color-button');
+const newColor = document.getElementById('new-color');
+const idBackground = document.getElementById('id-background');
+
+
+
+colorButton.addEventListener('click', () =>{
+    idBackground.style.backgroundColor = newColor.value;
+    count ++ ;
+    display.innerHTML = count ;
+    //console.log(newColor.value);
+
+});
+
+
+const pronounButton = document.getElementById('pronoun-button');
+const pronoun = document.getElementById('pronoun');
+const newPronoun = document.getElementById('new-pronoun');
+
+pronounButton.addEventListener('click', () =>{
+    count ++ ;
+    display.innerHTML = count ;
+    //console.log(newPronoun.value);
+    pronoun.textContent = newPronoun.value;
+});
+
